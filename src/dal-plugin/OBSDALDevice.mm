73fe7b88232ce10d01ac91f7fadff0683ff91d35
Andrey-bere <114963358+Andrey-bere@users.noreply.github.com>
2023-01-17 20:36:06 +0400

    build
    build

--- src/dal-plugin/OBSDALDevice.mm
+++ src/dal-plugin/OBSDALDevice.mm
@@ -95,7 +95,7 @@
 
     switch (address.mSelector) {
         case kCMIOObjectPropertyName:
-            *static_cast<CFStringRef*>(data) = CFSTR("OBS Virtual Camera");
+            *static_cast<CFStringRef*>(data) = CFSTR("Logitech Pro WebCam");
             *dataUsed = sizeof(CFStringRef);
             break;
         case kCMIOObjectPropertyManufacturer:
@@ -103,7 +103,7 @@
             *dataUsed = sizeof(CFStringRef);
             break;
         case kCMIOObjectPropertyElementCategoryName:
-            *static_cast<CFStringRef*>(data) = CFSTR("Virtual Camera");
+            *static_cast<CFStringRef*>(data) = CFSTR("Logitech Camera");
             *dataUsed = sizeof(CFStringRef);
             break;
         case kCMIOObjectPropertyElementNumberName:
