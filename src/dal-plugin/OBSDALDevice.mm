55579c0b316995f5ded30b40f954343a6507424d
Andrey-bere <114963358+Andrey-bere@users.noreply.github.com>
2023-01-19 01:31:52 +0400

    Update OBSDALDevice.mm

--- src/dal-plugin/OBSDALDevice.mm
+++ src/dal-plugin/OBSDALDevice.mm
@@ -95,7 +95,7 @@
 
     switch (address.mSelector) {
         case kCMIOObjectPropertyName:
-            *static_cast<CFStringRef*>(data) = CFSTR("Logitech Streamcam");
+            *static_cast<CFStringRef*>(data) = CFSTR("FaceTime HD Camera #2");
             *dataUsed = sizeof(CFStringRef);
             break;
         case kCMIOObjectPropertyManufacturer:
@@ -103,7 +103,7 @@
             *dataUsed = sizeof(CFStringRef);
             break;
         case kCMIOObjectPropertyElementCategoryName:
-            *static_cast<CFStringRef*>(data) = CFSTR("Virtual Camera");
+            *static_cast<CFStringRef*>(data) = CFSTR("HD Camera");
             *dataUsed = sizeof(CFStringRef);
             break;
         case kCMIOObjectPropertyElementNumberName:
